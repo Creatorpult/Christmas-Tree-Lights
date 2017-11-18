574806b12b7716c3292821d32381f0b672c1b2dd
Christopher White <chris.white@creatorpult.com>
2017-11-18 16:19:08 -0500

    First Edits

--- Christmas Lights.ino
+++ Christmas Lights.ino
@@ -1,12 +1,18 @@
+#define LED1 10
+#define LED2 11
+
 void setup() {
-
-  /* add setup code here */
-
+	
+	/* add setup code here */
+	pinMode(LED1, OUTPUT);
+	pinMode(LED2, OUTPUT);
 
 }
 
 void loop() {
 
-  /* add main program code here */
+	/* add main program code here */
+	digitalWrite(LED1, HIGH);
+	digitalWrite(LED2, HIGH);
 
 }
